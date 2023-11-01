70d5f2a139958908329c4a5618dab1c0c8d0bd87
仇公海 <qgh@meitu.com>
2023-11-01 10:20:12 +0800

    init

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600
   })
 
@@ -11,4 +11,18 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+
+  // 即使没有打开任何窗口，macOS 应用通常也会继续运行。 在没有窗口可用时调用 app 会打开一个新窗口。
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// 在 Windows 和 Linux 上，我们通常希望在关闭一个应用的所有窗口后让它退出。
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
